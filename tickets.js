fd92edb72be7b60b2c9a0cbb6589d23bb52c4edd
Joel Smith <jsumnersmith@gmail.com>
2020-11-16 15:53:49 -0500

    Added age of oldest ticket:

--- tickets.js
+++ tickets.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const moment = require('moment');
 require('dotenv').config();
 
 const getTicketsByTag = async labelId => {
@@ -19,14 +20,27 @@ const getTicketsByTag = async labelId => {
   .then(data => data.filter(datum => datum.workflow_state_id !== 500005660));  
 }
 
+const getOldestTicket = async (tickets, unit) => {
+  if (tickets.length < 1) return null;
+  let oldestTicket = await tickets.sort((ticket1, ticket2) => Date(ticket2.created_at) - Date(ticket1.created_at)).slice(-1)[0];
+  let diff = new Date() - new Date(oldestTicket.created_at);
+  if (unit == 'days') {
+    return Math.round(new moment.duration(diff).asDays(), 0);
+  } else if (unit ='hours') {
+    return Math.round(new moment.duration(diff).asHours(), 0);
+  }
+}
+
 const getTicketCount = async () => {
-  console.log("Firing up.")
-  let p0bugs = await getTicketsByTag(15201);
   let p1bugs = await getTicketsByTag(15200);
   let p2bugs = await getTicketsByTag(15199);
   let p3bugs = await getTicketsByTag(15198);
-  
-  console.log(p0bugs, p1bugs, p2bugs, p3bugs);
+  let p4bugs = await getTicketsByTag(16661);
+
+  let p1age = await getOldestTicket(p1bugs, 'hours');
+  let p2age = await getOldestTicket(p2bugs, 'days');
+  let p3age = await getOldestTicket(p3bugs, 'days');
+  let p4age = await getOldestTicket(p4bugs, 'days');
 
   return await {
     "blocks": [
@@ -34,14 +48,14 @@ const getTicketCount = async () => {
     		"type": "section",
     		"text": {
     			"type": "mrkdwn",
-    			"text": `• \`${p0bugs.length}\` P0 bugs open\n• \`${p1bugs.length}\` P1 bugs open\n• \`${p2bugs.length}\` P2 bugs open\n• \`${p3bugs.length}\` P3 bugs open`
+          "text": `• \`${p1bugs.length}\` P1 bugs open${ p1age ? ` | The oldest is ${p1age} hours old. Must resolve ASAP` : ``}\n• \`${p2bugs.length}\` P2 bugs open${ p2age ? ` | The oldest is ${p2age} days old. (max 14 days) ` : ``}\n• \`${p3bugs.length}\` P3 bugs open${ p3age ? ` | The oldest is ${p3age} days old (max 24 days)` : ``}\n• \`${p4bugs.length}\` P4 bugs open${ p4age ? ` | The oldest is ${p4age} days old (no max)` : ``}`
     		}
       },
       {
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": `<https://app.clubhouse.io/gatsbyjs/stories/space/17007/b-d-cs-only|:standup-clubhouse: View Board>`
+          "text": `:point_right: <https://app.clubhouse.io/gatsbyjs/stories/space/17007/b-d-cs-only|:standup-clubhouse: View Board>`
         }
       }
     ]
